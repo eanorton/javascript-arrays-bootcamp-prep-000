73e94c5b5145eb3eb2bc84f0c086f62b46bca2fe
Edward Norton <edwardnorton@icloud.com>
2017-12-21 22:40:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array
 }
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
